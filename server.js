151157c211b3ff5ef5da32eda96f23d4be206d98
indina853 <klauskl33in@gmail.com>
2025-11-13 13:22:48 -0300

    reasssssssaaaas

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error("❌ Erro MongoDB:", err));
 
 // Rotas
-app.use("/api/motoboys", motoboyRoutes);
-app.use("/api/pedidos", pedidoRoutes);
+app.use("/motoboys", motoboyRoutes);
+app.use("/pedidos", pedidoRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
