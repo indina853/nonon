5dfea328aed985e1d81c40e89c83c6c5a3c99819
indina853 <klauskl33in@gmail.com>
2025-11-13 12:48:04 -0300

    rdssss

---
+++ build/static/js/488.d86f7422.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdelivery_frontend=self.webpackChunkdelivery_frontend||[]).push([[488],{488:(e,n,k)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.d86f7422.chunk.js.map
